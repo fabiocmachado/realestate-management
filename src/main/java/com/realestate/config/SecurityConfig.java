f4ef41bfd9cde463a4b2d90170de431d40ad7816
Fabio Machado <camposmachado.fabio@gmail.com>
2025-01-26 09:33:18 +0000

    fix: railway route add.

--- src/main/java/com/realestate/config/SecurityConfig.java
+++ src/main/java/com/realestate/config/SecurityConfig.java
@@ -58,7 +58,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000", // Frontend local
+                "https://realestate-management-frontend.up.railway.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
